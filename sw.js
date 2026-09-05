a4b8fc8de511dcfe640880d007d5af9f53df397f
wang66676 <691288299@qq.com>
2026-09-05 10:58:26 +0800

    贷款口径精确对齐 v1.2 (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'car-est-v2';
+const CACHE = 'car-est-v3';
 const ASSETS = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-180.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
